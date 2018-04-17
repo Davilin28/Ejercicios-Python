e94c7b1c91c0bd1f37b0fffa1e191c9b919c39cd
David Román <davidromanrey28@gmail.com>
2018-04-17 10:58:23 +0200

    Add files via upload

---
+++ validador.py
@@ -0,0 +1,9 @@
+import contra
+
+correcto=True
+
+while correcto==True:
+    contrasenia=input("Ingrese su Password: ")
+    if passw.clave(contrasenia)==True:
+        print("Contraseña Aceptada")
+        correcto=False
